47e31096bd825c48bc4c5c7a3749903f3caf5739
omarelalami <83217646+omarelalami@users.noreply.github.com>
2021-11-18 16:43:50 +0100

    Update Ellipse.cs

--- Form/Ellipse.cs
+++ Form/Ellipse.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,20 +6,63 @@ namespace Form
 {
     class Ellipse
     {
-        Point C;
-        Point A;
-        Point B;
-        Droit r;
-        Droit R;
-        Ellipse(Droit r,Droit R) {
+        protected Point c;
+        protected Point ar;
+        protected Point bR;
+        protected Droit r;
+        protected Droit R;
 
-            r = new Droit(C, A);
-            R = new Droit(C, B);
-            this.r = r;
-            this.R = R;
+       public Ellipse(Droit r ,Droit R,Point c,Point ar,Point bR)
+        { this.c = c;
+          this.ar = ar;
+          this.bR = bR;
+           r = new Droit(c, ar);
+           R = new Droit(c, bR);
+          this.r = r ;
+          this.R = R;
            
-            }
+        }
 
+    public double Distance(Ellipse S)
+        {
+            double d = c.Distance(S.c);
+                     
+            return d;
+        }
+     public bool Intersection(Ellipse S)
+        {
+            bool b = false;
+
+            if (S.c.Distance(c) > S.bR.Distance(S.c) + bR.Distance(c))
+                b = false;
+            else { b = true; }
+            return b;
+        }
+
+    public bool Egalité(Ellipse S)
+        {
+            bool b = false;
+            if (S.r.EgalitéD(r) && S.R.EgalitéD(R))
+                b = true;
+            return b;
+        }
+
+        public double Surface()
+        {
+            const double Pi = 3.14;
+            double s = c.Distance(bR) * c.Distance(ar) * Pi;
+            return s;
+
+        }
+
+        public double Périmetre()
+        { const double Pi = 3.14;
+            double p=Pi*Math.Sqrt( 2*(Math.Pow(c.Distance(ar),2)) +Math.Pow(c.Distance(ar),2));
+
+            return p;
+
+
+        }
 
 
         
