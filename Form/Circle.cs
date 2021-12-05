a84bdcd660b098865945c43e6790630dc93efa96
omarelalami <83217646+omarelalami@users.noreply.github.com>
2021-12-05 18:38:57 +0100

    Update Circle.cs

--- Form/Circle.cs
+++ Form/Circle.cs
@@ -1,10 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Form
 {
-    class Circle
+    /// <summary>
+    /// La circle c'est un cas particulier de la class ellipse donc elle hérite dans cette class
+    /// et pour tout les fonction de la class ellipse c'est appliquent aussi dans la classe circle
+    /// </summary>
+    class Circle : Ellipse 
     {
-    }
-}
+        /// <summary>
+        /// Constructeur Qui initialise les paramétres
+        /// </summary>
+        /// <param name="c">Le centre de cercle </param>
+        /// <param name="bR"> le point qui définit la distance avec c (LE RAYON)</param>
+      public   Circle( Point c ,Point bR) 
+            : base (c,bR,bR)
+      
+        {
+            base.c = c;
+            base.bR = bR;
+            base.ar = bR;
+        } 
+
+
+
+       
+        
+    }}
