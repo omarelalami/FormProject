8d6bd2ae5cec3764eceba73a9ab1075ce8c94a29
omarelalami <83217646+omarelalami@users.noreply.github.com>
2021-12-05 18:40:00 +0100

    Update Droit.cs

--- Form/Droit.cs
+++ Form/Droit.cs
@@ -5,14 +5,22 @@ using System.Text;
 namespace Form
 {
     class Droit
-    {
+    {   /// <summary>
+    /// Le premier pointe de droite
+    /// </summary>
         private Point A;
+        /// <summary>
+        /// La dexieme point de droit
+        /// </summary>
         private Point B;
 
 
-
-       public  Droit(Point A, Point B)
-        {
+       /// <summary>
+       /// LE CONSTRUCTEUR INTIALISE LES DEUX POINT
+       /// </summary>
+       /// <param name="A"></param>
+       /// <param name="B"></param>
+       public  Droit(Point A, Point B) {
 
             this.A = A;
             this.B = B;
@@ -20,17 +28,30 @@ namespace Form
          
 
         }
-
- 
+        /// <summary>
+        /// La fontion vérifier si le droit en cours égale un autre droite et par la comparaison des point
+        /// </summary>
+        /// <param name="D"> le droit qui la fonction compare </param>
+        /// <returns> si les droit sont égaux alors retourne true sinon false</returns>
         public bool EgalitéD(Droit D)
         {
             bool b= false;
 
-            if (A.Egalité(D.A) && B.Egalité(D.B) || A.Egalité(D.B) && B.Egalité(D.A))
+            
+            double cof1 = (B.getCordY() - A.getCordY() )/( B.getCordX() - A.getCordX());
+            double cof2 = (D.B.getCordX() - D.A.getCordX()) / (D.B.getCordX() - D.A.getCordX());
+            double b1 = B.getCordY() - cof1 * B.getCordX();
+            double b2 = D.B.getCordY() - cof1 * D.B.getCordX();
+            if ((cof1==cof2) && (b1==b2))
                 b = true;
 
             return b;
-        }
+        }    
+        /// <summary>
+        /// Pour vérifier L'intersection de deux droit et pour ça en utilise une formule qui vérifier cette opération
+        /// </summary>
+        /// <param name="D"> Le droit qui véifier si il ya un intersection avec le droit en cours</param>
+        /// <returns> retourne True si il y a un intersection sino false</returns>
         public bool Interection(Droit D)
         { bool b = false;
 
@@ -40,7 +61,7 @@ namespace Form
                 b = true;
 
             return b;
-        }
+        }  
         public bool Parallélisme(Droit D)
         {
             bool b = false;
@@ -48,14 +69,18 @@ namespace Form
                 b = true;
             return b;
         }
-
+        
         public bool Appartenance (Point p)
         { bool b = false;
             if ((p.getCordX() - A.getCordX()) * (A.getCordY() - B.getCordY()) == (p.getCordY() - A.getCordY()) * (A.getCordX() - B.getCordX()))
                  b = true;
             return b;
-        }
+        }///
 
+        public double Longeur()
+        {
+            return A.Distance(B);
+        } 
 
 
 
